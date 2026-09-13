135bd841174718638f211596702e43e4a094b78d
jomadeto <jomadeto@gmail.com>
2026-09-13 15:11:33 +0200

    dolby: Remove DSPVolumeSynchronizer Duplicated in android_hardware_xiaomi

--- dolby.mk
+++ dolby.mk
@@ -99,10 +99,6 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_PACKAGES += \
     RemovePackagesDolby
 
-# DSP Volume Synchronizer
-PRODUCT_PACKAGES += \
-    DSPVolumeSynchronizer
-
 # Motorola bloatwares
 PRODUCT_PACKAGES += \
     MotoSignatureApp \
